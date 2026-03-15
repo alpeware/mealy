5dbe16b141724db69ef4184d49797d7899e7ec78
alpeware <info@alpeware.com>
2026-03-15 14:11:20 -0500

    Create test-runner.clj

---
+++ test/test-runner.clj
@@ -0,0 +1,10 @@
+(ns mealy.test-runner
+  "Test runner for Mealy"
+  (:require [clojure.test :as t]
+            [mealy.core]))
+
+(defn -main
+  "Main entry point for running tests"
+  [& _args]
+  (let [results (t/run-all-tests #"mealy.*")]
+    (System/exit (+ (:fail results) (:error results)))))
